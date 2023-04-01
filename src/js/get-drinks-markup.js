34ea49bcf55c240bc60a52ec477d066f75e21665
Katrin9959 <122272747+Katrin9959@users.noreply.github.com>
2023-04-02 00:30:24 +0200

    modal-js

--- src/js/get-drinks-markup.js
+++ src/js/get-drinks-markup.js
@@ -1,23 +1,34 @@
+import { showDrinkModal } from "./modal-cocktails";
 export function getDrinksMarkup(array) {
-    const markup = array.map(({ strDrink, strDrinkThumb }) => {
-        return getCardMarkup(strDrink, strDrinkThumb)
+    const markup = array.map(({ strDrink, strDrinkThumb, idDrink }) => {
+        return getCardMarkup(strDrink, strDrinkThumb, idDrink)
     }).join('');
 
     return markup;
 }
 
 
-function getCardMarkup(name, imageSrc) {
+function getCardMarkup(name, imageSrc, id) {
     return (`
 <li class="cocktails__item">
     <img class="cocktails__img" src="${imageSrc}" alt="cocktail"/>
     <div class="cocktails__descr">
         <h3 class="cocktails__name">${name}</h3>
         <div class="cocktails__btns">
-            <button class="btn__learn" type="button">Learn more</button>
+            <button class="btn__learn" type="button" name="learn-more" data-id="${id}" >Learn more</button>
             <button class="btn__delete" type="button">Remove</button>
         </div>
     </div>
 </li>
 `)
+}
+export function handleCardClick(event) {
+    
+    if (event.target.name === 'learn-more') {
+        const id = event.target.dataset.id;
+        showDrinkModal(id);
+    }
+    if (event.target.name === 'addRemove') {
+        event.target.classList.toggle('btn__delete');
+    }
 }
--- src/js/modal-cocktails.js
+++ src/js/modal-cocktails.js
@@ -1,65 +1,53 @@
-const addToModal = document.querySelectorAll('.addToModal');
-
-if(addToModal.length > 0) {
-    addToModal.forEach(function(btnLearnMore){
-        btnLearnMore.addEventListener('click', viewCocktailDetail);
+import { fetchCocktailById } from "./fetch-cocktails";
+const drinkModal = document.querySelector( '#modal-drink-detail')
+export function showDrinkModal(id) {
+    fetchCocktailById(id)
+    .then(data => {
+        drinkModal.innerHTML = getDrinkModalMarkup(data);
+        const closeCocktailBtn = drinkModal.querySelector( '.modal__button');
+        const ingredientList = drinkModal.querySelector('.ingredients__list');
+        ingredientList.innerHTML= getIngredientsList(data);
+        closeCocktailBtn.addEventListener('click', closeCocktailModal);
+drinkModal.classList.toggle('show-modal')
     })
 }
 
-
-// Виводимо інфомрацію про коктейль
-function viewCocktailDetail() {
-
-    // Отримуємо id коктейлю по якому був клік
-    const cocktailId = this.getAttribute('data-product-id');
-
-    // Отримуємо об'єкт з даними
-    const cocktailDetail = getCocktailDetail(cocktailId);
-    
-    // Проходимося по промісу, який прийшов з функції
-    cocktailDetail.then(details => {
-        
-        let cocktail = `<button class="modal__button" data-modal-close>
+function getDrinkModalMarkup(details){
+    return (`
+    <button class="modal__button" data-modal-close>
                 <svg class="modal-icon-close" width="18" height="18">
                 <use href=""></use>
                 </svg>
             </button>
             <div class="modal__cocktail">
-                <strong class="modal__title">${details.strDrink}</strong>
-                <h2 class="modal__instractions">INSTRACTION:</h2>
-                <p class="paragraf">${details.strInstructions}</p>
+                <h3 class="modal__title">${details.strDrink}</h3>
+                <h4 class="modal__instructions">INSTRUCTION:</h4>
+                <p class="instruction">${details.strInstructions}</p>
                 <img class="modal1__picture" src="${details.strDrinkThumb}" alt="" width="280" height="280" />
-                <h2> INGREDIENTS</h2>
-                <h3> Per cocktail</h3>
-                <ul>`;
-        
-        for (let i = 1; i <= 15; i++) {
-            let ingridient = details['strIngredient'+i];
-            
-            if (ingridient) {
-                cocktail += `<li>${ingridient}</li>`;
-            } else {
-                break;
-            }
-        }
-
-        cocktail += `</ul>
+                <h4> INGREDIENTS</h4>
+                <p> Per cocktail</p>
+                <ul class="ingredients__list" ></ul>
             <button class="modal__button-add" data-id='${details.idDrink}' type="button" data-modal-add> Add to favorite </button>
             <button class="modal__button-remove" data-id='${details.idDrink}' type="button" data-modal-remove> Remove from favorite </button>
-        </div>`;
-
-        document.querySelector('#modal-drink-detail').innerHTML = cocktail;
-    });
-
-
+        </div>
+    `)
+}
+function getIngredientsList(details){
+    let ingredients =''
+    for (let i = 1; i <= 15; i++) {
+        let ingredient = details['strIngredient'+i];
+        
+        if (ingredient) {
+            ingredients += `<li class= "ingredient__item">${ingredient}</li>`;
+        } else {
+            break;
+        }
+    }
+    return ingredients
 }
 
-
-function getCocktailDetail(cocktailId) {
-
-    // Формуємо посилання до API
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`;
-    
-    // Звертаємо до api і отримуємо відповідь
-    return  fetch(url).then(response => response.json()).then(({ drinks } ) => { return drinks[0]; });
+function closeCocktailModal(){
+    drinkModal.innerHTML = '';
+    drinkModal.classList.toggle('show-modal');
 }
+
