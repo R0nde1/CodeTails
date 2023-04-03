32af9d11d471ea7c3cba75f58c3b20b7bf478d8f
R0nde1 <122411582+R0nde1@users.noreply.github.com>
2023-04-03 13:24:15 +0000

    Deploying to gh-pages from @ R0nde1/CodeTails@27efd49a2ae2557f001306bcd56b4e5bc7fcbb38 🚀

---
+++ index.1f2c8bb7.js
@@ -0,0 +1,2 @@
+!function(){"use strict";document.querySelector(".search-form__wrapper").addEventListener("input",e);document.querySelector(".fav-ingredients");function e(){console.log(333)}document.querySelectorAll(".btn__delete").addEventListener("click",e)}();
+//# sourceMappingURL=index.1f2c8bb7.js.map
---
+++ index.bd4cee90.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form__wrapper").addEventListener("input",e);document.querySelector(".fav-ingredients");function e(){console.log(333)}document.querySelectorAll(".btn__delete").addEventListener("click",e);
+//# sourceMappingURL=index.bd4cee90.js.map
