e76608695940cd4d01f7601f02ae1dc07d3b29c2
R0nde1 <122411582+R0nde1@users.noreply.github.com>
2023-04-03 23:35:15 +0300

    Update letters.js

--- src/js/letters.js
+++ src/js/letters.js
@@ -12,6 +12,8 @@ const letterOnSelect = document.querySelector('.select');
 symbolPanel.addEventListener('click',  getCocktails1);
 letterOnSelect.addEventListener('click',  getCocktails1);
 
+let letterSearchResults;
+
 function getCocktails1(ev) {
         if (!ev.target.dataset.value) {
        return; 
@@ -24,7 +26,7 @@ function getCocktails1(ev) {
   
     fetchCocktailsByLetter(cocktailFirstLetter)
       .then(data => {
-        searchResults = data;
+        letterSearchResults = data;
         contentTitle.innerHTML = 'Cocktails';
         contentResults.append ( ...getDrinksMarkup(
         data.slice(0, numberOfCocktails)
@@ -45,7 +47,7 @@ function getCocktails1(ev) {
 function changePage1(e) {
     const page = Number(e.target.dataset.page);
     const numberOfCocktails = calculateCocktails();
-    const data = searchResults;
+    const data = letterSearchResults;
     const end = page * numberOfCocktails;
     const start = end - numberOfCocktails;
     contentResults.innerHTML = '';
