d01efd46992abeda41117cf6b05fbf28188ce02f
Tanya <tkravche@yahoo.com>
2023-04-03 18:19:41 +0200

    pagination for search without dots

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,16 +4,32 @@ export function createPagination(totalResults, pageSize, pageNumber) {
     return '';
   }
   let inactive = pageNumber === 1 ? ' inactive' : '';
-  let result = `<li class="pagination__number left-arrow${inactive}" data-page="${
+  let result = `<div class = "pagination-line"><li class="pagination__number left-arrow${inactive}" data-page="${
     pageNumber - 1
   }">&lt;</li>`;
+  const pagesToShow = [
+    1,
+    pageNumber - 1,
+    pageNumber,
+    pageNumber + 1,
+    pagesCount,
+  ];
+  let dotsAdded = false;
   for (let i = 1; i <= pagesCount; i++) {
     const active = i === pageNumber ? ' inactive' : '';
-    result += `<li class="pagination__number${active}" data-page="${i}">${i}</li>`;
+    
+    if (pagesToShow.includes(i)) {
+      result += `<li class="pagination__number${active}" data-page="${i}">${i}</li>`;
+      dotsAdded = false;
+    }
+    else if(!dotsAdded) {
+      result += `<li class="pagination__number inactive dots">...</li>`;
+      dotsAdded = true;
+    }
   }
   inactive = pageNumber === pagesCount ? ' inactive' : '';
   result += `<li class="pagination__number right-arrow${inactive}" data-page="${
     pageNumber + 1
-  }">&gt;</li>`;
+  }">&gt;</li></div>`;
   return result;
 }
