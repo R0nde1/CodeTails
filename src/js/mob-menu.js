b058768cfc23fee12f7191fab8aa714332ff0d34
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-04-05 20:44:21 +0300

    Update mob-menu.js

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,4 +1,3 @@
-(() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -15,4 +14,3 @@
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
   });
-})();
