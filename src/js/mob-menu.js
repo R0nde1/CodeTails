dafadb64e91bc1dde2b97c34ba5f5140ea61601d
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-04-05 18:07:09 +0300

    correction

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -4,8 +4,6 @@
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
   };
 
