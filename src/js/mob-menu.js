a2cecc54384896286af20c902371ae0b6bd75ac0
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-04-05 21:09:13 +0300

    Update mob-menu.js

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,16 +1,16 @@
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+const mobileMenu = document.querySelector('.js-menu-container');
+const openMenuBtn = document.querySelector('.js-open-menu');
+const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const toggleMenu = () => {
-    mobileMenu.classList.toggle('is-open');
-  };
+const toggleMenu = () => {
+  mobileMenu.classList.toggle('is-open');
+};
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
 
-  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-  });
+window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  mobileMenu.classList.remove('is-open');
+  openMenuBtn.setAttribute('aria-expanded', false);
+});
