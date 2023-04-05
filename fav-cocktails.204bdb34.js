7b6e3e9e02353c1306a8771c8b6ab80aef5311b0
R0nde1 <122411582+R0nde1@users.noreply.github.com>
2023-04-05 17:33:27 +0000

    Deploying to gh-pages from @ R0nde1/CodeTails@5728bb19f02753c2c64bea75206e64ced0eff19e 🚀

---
+++ fav-cocktails.204bdb34.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("#modal-footer"),n=document.getElementById("open-modal-footer"),t=e.querySelector(".modal__button");n.addEventListener("click",(function(){e.style.display="block"})),t.addEventListener("click",(function(){e.style.display="none"})),window.onclick=function(n){n.target==e&&(e.style.display="none")}}();
+//# sourceMappingURL=fav-cocktails.204bdb34.js.map
---
+++ fav-cocktails.85ee586f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#modal-footer"),t=document.getElementById("open-modal-footer"),n=e.querySelector(".modal__button");t.addEventListener("click",(function(){e.style.display="block"})),n.addEventListener("click",(function(){e.style.display="none"})),window.onclick=function(t){t.target==e&&(e.style.display="none")};
+//# sourceMappingURL=fav-cocktails.85ee586f.js.map
