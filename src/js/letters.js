a0d8223d5b433f3056cbcf793d14891e84894065
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-04-05 20:37:07 +0200

    Невеликі правки по зауваженню по перемінним. Видалення порожніх файлів html

--- src/js/letters.js
+++ src/js/letters.js
@@ -9,16 +9,15 @@ const contentTitle = document.querySelector('#content-title');
 const symbolPanel = document.querySelector('.symbol__panel');
 const letterOnSelect = document.querySelector('.select');
 
-symbolPanel.addEventListener('click', getCocktails1);
-letterOnSelect.addEventListener('click', getCocktails1);
+symbolPanel.addEventListener('click', getCocktailsL);
+letterOnSelect.addEventListener('click', getCocktailsL);
 
-let letterSearchResults;
 
-function getCocktails1(ev) {
+function getCocktailsL(ev) {
   if (!ev.target.dataset.value) {
     return;
   }
-  let cocktailFirstLetter = ev.target.dataset.value;
+  const cocktailFirstLetter = ev.target.dataset.value;
 
   contentTitle.innerHTML = 'Searching results..';
   contentResults.innerHTML = '';
@@ -26,7 +25,6 @@ function getCocktails1(ev) {
 
   fetchCocktailsByLetter(cocktailFirstLetter)
     .then(data => {
-      letterSearchResults = data;
       contentTitle.innerHTML = 'Cocktails';
       contentResults.append(
         ...getDrinksMarkup(data.slice(0, numberOfCocktails))
